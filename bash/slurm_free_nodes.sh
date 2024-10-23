97a66ebc5b5bc7a819658f28f6e01dc1168d8654
ehedlin <iamerichedlin@gmail.com>
2024-10-23 12:15:07 -0700

    Update slurm_free_nodes.sh
    fixing available memory bug

--- bash/slurm_free_nodes.sh
+++ bash/slurm_free_nodes.sh
@@ -9,17 +9,23 @@ while IFS= read -r node; do
   node_name=$(echo "$node_info" | grep -oP "(?<=NodeName=)\S+")
   total_cpus=$(echo "$node_info" | grep -oP "(?<=CPUTot=)\S+")
   alloc_cpus=$(echo "$node_info" | grep -oP "(?<=CPUAlloc=)\S+" || echo "0")
-  free_memory=$(echo "$node_info" | grep -oP "(?<=FreeMem=)\S+")
+  total_memory=$(echo "$node_info" | grep -oP "(?<=RealMemory=)\S+")
+  alloc_memory=$(echo "$node_info" | grep -oP "(?<=AllocMem=)\S+" || echo "0")
+  
+  # Calculate free memory and convert memory from MB to GB
+  free_memory=$((total_memory - alloc_memory))
+  free_memory_gb=$(echo "scale=2; $free_memory/1024" | bc)
+  total_memory_gb=$(echo "scale=2; $total_memory/1024" | bc)
+  
   total_gpus=$(echo "$node_info" | grep "Gres=" | grep -oP "gpu:[^:]+:\K[0-9]+")
   alloc_gpus=$(echo "$node_info" | grep "AllocTRES=" | grep -oP "gres/gpu=\K[0-9]+" || echo "0")
   gpu_type=$(echo "$node_info" | grep -oP "(?<=AvailableFeatures=)\S+")
   users=$(squeue --nodes=$node_name --noheader --format="%u" | sort | uniq | paste -sd, -)
 
-
   # Calculate available resources
   free_cpus=$((total_cpus - alloc_cpus))
   free_gpus=$((total_gpus - alloc_gpus))
 
-  # Print the information
-  echo "$node_name available resources: $free_gpus/$total_gpus GPUs, $free_cpus/$total_cpus CPUs, ${free_memory}M memory, $gpu_type, users:$users"
+  # Print the information with memory in GB
+  echo "$node_name available resources: $free_gpus/$total_gpus GPUs, $free_cpus/$total_cpus CPUs, ${free_memory_gb}GB/${total_memory_gb}GB memory, $gpu_type, users:$users"
 done < <(sinfo -N -o "%N %G" --noheader | grep "gpu:" | awk '{print $1}')
